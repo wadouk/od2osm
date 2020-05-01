cdb66b5c6fca942bbf9016b54a0a0f30ba35f630
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-01 17:29:10 +0200

    handle auth

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,8 +1,10 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser exposing (Document, application)
 import Browser.Navigation exposing (Key)
+import Dict exposing (Dict)
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Url exposing (Protocol(..), Url)
 
 
@@ -12,7 +14,7 @@ main =
         { init = init
         , update =
             update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         , onUrlRequest = always NoOp
         , onUrlChange = always NoOp
         , view = view
@@ -23,20 +25,76 @@ type alias Model =
     { navigationKey : Key
     , flags : Flags
     , origin : Url
+    , token: Maybe String
     }
 
 init : Flags -> Url -> Key -> ( Model, Cmd Msg )
-init mflags origin navigationKey = (Model navigationKey mflags origin, Cmd.none)
+init mflags origin navigationKey =
+    let
+        q = queryParameters origin
+        token = Dict.get "oauth_token" q |> Maybe.map List.head |> Maybe.withDefault Nothing
+    in (Model navigationKey mflags origin token, Cmd.none)
 
 type alias Flags = {}
 
 type Msg = NoOp
+    | Authenticate
+    | Authenticated
+
+type Route =
+    Home (Maybe String)
+
+queryParameters : Url -> Dict String (List String)
+queryParameters url =
+    let
+        toTuples : String -> List ( String, String )
+        toTuples str =
+            case String.split "=" str of
+                key :: value -> [ ( key, String.join "=" value ) ]
+                [] -> []
+
+        toDict : List ( String, String ) -> Dict String (List String)
+        toDict parameters =
+            List.foldl
+                (\( k, v ) dict -> Dict.update k (addParam v) dict)
+                Dict.empty
+                parameters
+
+        addParam : String -> Maybe (List String) -> Maybe (List String)
+        addParam value maybeValues =
+            case maybeValues of
+                Just values -> Just (value :: values)
+                Nothing -> Just [ value ]
+    in
+    url.query
+        |> Maybe.andThen Url.percentDecode
+        |> Maybe.map (String.split "&" >> List.concatMap toTuples >> toDict)
+        |> Maybe.withDefault Dict.empty
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model = (model, Cmd.none)
+update msg model =
+    case msg of
+        NoOp -> (model, Cmd.none)
+        Authenticate -> (model, authenticate())
+        Authenticated -> (model, Cmd.none)
 
 view : Model -> Document Msg
 view model =
-    Document "OpenData 2 OSM" ([div [] [ text "hello"]])
+    Document "OpenData 2 OSM"
+        (Maybe.map (viewAlreadyLogin model) model.token |> Maybe.withDefault (viewLogin model))
 
+viewLogin model =
+    [ button [ onClick Authenticate] [ text "Login" ]
+    ]
 
+viewAlreadyLogin model token =
+    [ span [] [ text token ]
+    , button [] [ text "Fetch details"]
+    ]
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = authenticated (always Authenticated)
+
+port authenticate : () -> Cmd msg
+
+port authenticated : (() -> msg) -> Sub msg
