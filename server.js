bdce85479078cc819a208e96ac436aecfc3af5d7
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-04 17:10:56 +0200

    upload data

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
 'use strict'
 
-const Path = require('path')
 const Hapi = require('@hapi/hapi')
 const Inert = require('@hapi/inert')
 
-const {Client, types} = require('pg')
+const {Client, types, Pool} = require('pg')
 const client = new Client()
 
-var hstore = require('hstore.js')
+const pool = new Pool()
 
-const dbinit = async () => {
+const hstore = require('hstore.js')
+
+const dbHstoreType = async () => {
   try {
-    await client.connect()
-    console.log(1)
-    const oids = await client.query("SELECT oid FROM pg_type WHERE typname='hstore'")
-    console.log(2)
+    const oids = await pool.query("SELECT oid FROM pg_type WHERE typname='hstore'")
     types.setTypeParser(oids.rows[0].oid, function (val) {
       return hstore.parse(val)
     })
   } catch (e) {
     console.error(e)
   }
-
-  return client
-
 }
 
-const init = async () => {
+const start = async () => {
 
   const server = new Hapi.Server({
     port: 3000,
@@ -34,7 +29,7 @@ const init = async () => {
 
   await server.register(Inert)
 
-  const client = await dbinit()
+  await dbHstoreType()
 
   server.route({
     method: 'GET',
@@ -43,7 +38,7 @@ const init = async () => {
       cors: true,
     },
     handler: async (request, h) => {
-      const res = await client.query('select * from quests')
+      const res = await pool.query('select * from quests')
       return res.rows
     },
   })
@@ -56,14 +51,48 @@ const init = async () => {
     handler: async (request, h) => {
       const {params} = request
       const {qid} = params
-      console.log({qid})
-      try {
-        const res = await client.query('select * from points where qid = $1', [qid])
-        return res.rows
-      } catch (e) {
-        console.error(e)
+      const res = await pool.query('select * from points where qid = $1', [qid])
+      return res.rows
+    },
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/quests',
+    options: {
+      payload: {
+        multipart: true,
+        parse: true,
+        output: 'data',
+        allow: 'multipart/form-data',
+      },
+      response: {
+        emptyStatusCode: 204,
+      },
+    },
+    handler: async (request, h) => {
+      const {payload} = request
+      const {geojson} = payload
+      const datas = JSON.parse(geojson.toString())
+
+      await pool.query('truncate table points')
+
+      for (const {id, geometry, properties} of datas.features.filter(({properties}) => Boolean(properties))) {
+        const {coordinates} = geometry
+        const p = hstore.stringify(properties)
+        const [x, y] = coordinates
+        const c = `(${x},${y})`
+
+        let params = [c, p, id, 1]
+        try {
+          await pool.query('insert into points (point, properties, id, qid) values ($1, $2, $3, $4)', params)
+        } catch (e) {
+          console.error(params, e)
+          break
+        }
       }
 
+      return h.response()
     },
   })
 
@@ -77,4 +106,9 @@ process.on('unhandledRejection', (err) => {
   console.trace(err)
 })
 
-init()
+process.on("SIGINT", async function () {
+  await pool.end()
+  process.exit()
+})
+
+start()
