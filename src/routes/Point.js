4a284b91f1d849c458a9fa2609e11247ee33f58b
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-05 10:03:49 +0200

    more stuff

--- src/routes/Point.js
+++ src/routes/Point.js
@@ -17,19 +17,25 @@ export default class Point extends Component {
     this.setState({point: d[0]})
   }
 
-  async fetchOverpass () {
+  getOverpassQuery() {
     const {point} = this.state
+    if (!point) {
+      return
+    }
     const {properties} = point
-    const mainTag = Object.entries(properties).filter(([k, v]) => ['shop', 'amenity'].indexOf(k) !== -1).map(([k, v]) => `"${k}"="${v}"`)[0]
     const bbox = new LatLng(point.point.y, point.point.x).toBounds(this.state.radius)
     const bboxOverpass = [bbox.getSouth(), bbox.getWest(), bbox.getNorth(), bbox.getEast()].map(v => v.toFixed(6)).join(', ')
-    let query = `[out:json] [timeout:25] ;
-    (
-     node [${mainTag}](${bboxOverpass});
-    );
-     out; >; out skel qt;`
+    const q = Object.entries(properties)
+      .filter(([k, v]) => ['shop', 'amenity'].indexOf(k) !== -1)
+      .map(([k, v]) => v.split(";")
+        .map(u => `node ["${k}"="${u}"](${bboxOverpass}); `).join("\n")
+      )[0]
 
-    console.log(query)
+    return `[out:json] [timeout:25] ; ( ${q} ); out; >; out skel qt;`
+  }
+
+  async fetchOverpass () {
+    const query = this.getOverpassQuery()
     let body = new URLSearchParams({
       data: query,
     })
@@ -46,7 +52,6 @@ export default class Point extends Component {
     const r = await fetch("http://overpass-api.de/api/interpreter", options)
 
     const d = await r.json()
-    console.log(d)
     this.setState({overpass: d})
   }
 
@@ -54,17 +59,8 @@ export default class Point extends Component {
     const {properties} = point
     const bbox = new LatLng(point.point.y, point.point.x).toBounds(this.state.radius)
     return <div>
-      <h1>{properties.name}</h1>
-      <div>Centre: {`${point.point.x}, ${point.point.y}`}</div>
-      <div>BBox : {bbox.toBBoxString()}</div>
       <h2>OpenData</h2>
       <div>{Object.entries(properties).sort(([k1], [k2]) => k1 > k2).map(([k, v]) => <div><b>{k}</b> = <i>{v}</i></div>)}</div>
-      <div>
-        <label htmlFor="radius">Radius</label>
-        <input id="radius" type="number" value={this.state.radius} onChange={(e) => this.setState({radius: e.target.value})}/>
-        <button onClick={this.fetchOverpass.bind(this)}>Conflation</button>
-      </div>
-
       <Map center={{lon: point.point.x, lat: point.point.y}} bounds={bbox} className={style.leafletContainer}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -72,8 +68,22 @@ export default class Point extends Component {
         />
         <Circle center={{lon: point.point.x, lat: point.point.y}} radius={1}/>
         <Rectangle bounds={bbox}/>
+        {this.renderCirclesOverpass()}
       </Map>
     </div>
+  }
+
+  renderCirclesElements({elements}) {
+    return elements.map(this.renderCirclesElement)
+  }
+
+  renderCirclesElement({lon, lat}) {
+    return <Circle center={{lon, lat}} radius={1}/>
+  }
+
+  renderCirclesOverpass() {
+    const {overpass} = this.state
+    return overpass ? this.renderCirclesElements(overpass) : []
   }
 
   renderElement = ({id, lat, lon, tags}) => {
@@ -81,7 +91,8 @@ export default class Point extends Component {
     const p0 = new LatLng(point.point.y, point.point.x)
     const p1 = new LatLng(lat, lon)
     return <div>
-      <div>{id}</div>
+      <div><a href={`https://www.openstreetmap.org/node/${id}`}>{id}</a></div>
+      <div><button>Rapprocher de ce point</button></div>
       <div>Distance: {p0.distanceTo(p1).toFixed(0)}m</div>
       <div>{Object.entries(tags).sort(([k1], [k2]) => k1 > k2).map(([k, v]) => <div><b>{k}</b> = <i>{v}</i></div>)}</div>
     </div>
@@ -89,14 +100,25 @@ export default class Point extends Component {
 
   renderOverpass({elements}) {
     return <div>
+      <div>Résultat : {elements.length}</div>
       {elements.map(this.renderElement)}
     </div>
   }
 
   render({qid, pid}, {point, overpass}) {
-    return <div>
+    return <div className={style.point}>
       {point ? this.renderPoint(qid, pid, point) : <div>loading</div>}
-      {overpass ? this.renderOverpass(overpass) : <div>loading</div>}
+      <div>
+        <h2>Overpass</h2>
+        <div>
+          <label htmlFor="radius">Radius</label>
+          <input id="radius" type="number" value={this.state.radius} onChange={(e) => this.setState({radius: e.target.value})}/>
+          <button onClick={this.fetchOverpass.bind(this)}>Conflation</button>
+          <div><i>{this.getOverpassQuery()}</i></div>
+          <button>Créer un point ici</button>
+        </div>
+        {overpass ? this.renderOverpass(overpass) : null}
+      </div>
     </div>
   }
 }
