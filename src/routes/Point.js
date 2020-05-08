7f1aee8bb63c96552962e727b5fc1253c4ad2a10
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-08 21:25:17 +0200

    improve layout

--- src/routes/Point.js
+++ src/routes/Point.js
@@ -5,26 +5,27 @@ import leafletCss from 'leaflet/dist/leaflet.css'
 import {Marker, Map, Rectangle, TileLayer, Popup} from 'react-leaflet'
 import {LatLng} from 'leaflet'
 
-import L from 'leaflet';
+import L from 'leaflet'
 import Loader from '../components/Loader'
-delete L.Icon.Default.prototype._getIconUrl;
+
+delete L.Icon.Default.prototype._getIconUrl
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+})
 
 const diffStatusText = {
   "=": "même valeur dans OSM et l'OD",
   "~": "l'OD compléte OSM",
   "+": "l'OD compléte OSM",
-  "*": "OSM gagne"
+  "*": "OSM gagne",
 }
 
 export default class Point extends Component {
   state = {
-    radius: 20
+    radius: 20,
   }
 
   async componentDidMount() {
@@ -37,7 +38,7 @@ export default class Point extends Component {
   getOverpassQuery() {
     const {point} = this.state
     if (!point) {
-      return
+      return ""
     }
     const {properties} = point
     const bbox = new LatLng(point.point.y, point.point.x).toBounds(this.state.radius)
@@ -45,13 +46,13 @@ export default class Point extends Component {
     const q = Object.entries(properties)
       .filter(([k, v]) => ['shop', 'amenity'].indexOf(k) !== -1)
       .map(([k, v]) => v.split(";")
-        .map(u => `node ["${k}"="${u}"](${bboxOverpass}); `).join("\n")
+        .map(u => `node ["${k}"="${u}"](${bboxOverpass}); `).join("\n"),
       )[0]
 
-    return `[out:json] [timeout:25] ; ( ${q} ); out meta;`
+    return `[out:json] [timeout:25] ;\n ( \n${q} \n); out meta;`
   }
 
-  async fetchOverpass () {
+  async fetchOverpass() {
     const query = this.getOverpassQuery()
     let body = new URLSearchParams({
       data: query,
@@ -67,29 +68,17 @@ export default class Point extends Component {
     }
 
     this.setState({loaderOverpass: true})
-
     const r = await fetch("http://overpass-api.de/api/interpreter", options)
 
     const d = await r.json()
-    this.setState({loaderOverpass: false})
-    this.setState({overpass: d})
+    this.setState({loaderOverpass: false, overpass: d})
   }
 
   renderPoint(point) {
     const {properties} = point
-    const bbox = new LatLng(point.point.y, point.point.x).toBounds(this.state.radius)
     return <div>
       <h2>OpenData</h2>
       <div>{this.renderTags(properties)}</div>
-      <Map center={{lon: point.point.x, lat: point.point.y}} bounds={bbox} className={style.leafletContainer}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker draggable={true} position={{lon: point.point.x, lat: point.point.y}} onMoveEnd={this.markerOpendataMoved.bind(this)}><Popup>Opendata</Popup></Marker>
-        <Rectangle bounds={bbox}/>
-        {this.renderCirclesOverpass()}
-      </Map>
     </div>
   }
 
@@ -123,9 +112,15 @@ export default class Point extends Component {
     const p1 = new LatLng(lat, lon)
 
     return <div>
-      <div><a href={`https://www.openstreetmap.org/node/${id}`}>{id}</a></div>
-      <div><button onClick={this.setAction('merge', {id, lat, lon, tags})} disabled={this.state.action}>Rapprocher de ce point</button></div>
-      <div>Distance: {p0.distanceTo(p1).toFixed(0)}m</div>
+      <div className={style.renderElement}>
+        <span>Distance: {p0.distanceTo(p1).toFixed(0)}m</span>
+        &nbsp;
+        <a href={`https://www.openstreetmap.org/node/${id}`}>{id}</a>
+        &nbsp;
+        <button onClick={this.setAction('merge', {id, lat, lon, tags})} disabled={this.state.action} className={style.conflateThis}>
+          Rapprocher de ce point
+        </button>
+      </div>
       <h3>Tags</h3>
       <div>{this.renderTags(tags)}</div>
       <h3>Contexte</h3>
@@ -136,7 +131,9 @@ export default class Point extends Component {
   renderTags(t, cb) {
     return Object.entries(t)
       .sort(([k1], [k2]) => k1 > k2 ? 1 : -1)
-      .map(([k, v]) => <div> {cb ? <span className={style.diffStatusText} title={diffStatusText[cb(k, v)]} aria-label={diffStatusText[cb(k, v)]}>{cb(k, v)}</span> : null} <b>{k}</b> : <i>{v}</i></div>)
+      .map(([k, v]) => <div> {cb ? <span className={style.diffStatusText} title={diffStatusText[cb(k, v)]}
+                                         aria-label={diffStatusText[cb(k, v)]}>{cb(k, v)}</span> : null}
+        <b>{k}</b> : <i>{v}</i></div>)
   }
 
   renderDiffTags() {
@@ -150,7 +147,7 @@ export default class Point extends Component {
     function diffState(k, v) {
       let keysFromOSM = keysT.indexOf(k) !== -1
       let keysFromOpenData = keysP.indexOf(k) !== -1
-      if (keysFromOSM && !keysFromOpenData ) {
+      if (keysFromOSM && !keysFromOpenData) {
         return "*"
       } else if (!keysFromOSM && keysFromOpenData) {
         return "+"
@@ -177,7 +174,12 @@ export default class Point extends Component {
     })()
 
     return (<div>
-      <div><b>Status des différences</b> : <i>{diffStatusText[fullDiff]}</i></div>
+      <h3>Status des différences</h3>
+      <div className={style.renderElement}>
+        <i>{diffStatusText[fullDiff]}</i>
+        <button onClick={this.setAction()}>Annuler</button>
+      </div>
+      <h3>Fusion des tags</h3>
       <div>{this.renderTags(newTags, diffState)}</div>
     </div>)
   }
@@ -205,24 +207,48 @@ export default class Point extends Component {
   renderAction() {
     const {action} = this.state
     switch (action) {
-      case 'add': return this.renderAdd()
-      case 'merge': return this.renderMerge()
-      default: return undefined
+      case 'add':
+        return this.renderAdd()
+      case 'merge':
+        return this.renderMerge()
+      default:
+        return undefined
     }
+  }
+
+  renderMap(point) {
+    const bbox = new LatLng(point.point.y, point.point.x).toBounds(this.state.radius)
+    return <Map center={{lon: point.point.x, lat: point.point.y}} bounds={bbox} className={style.leafletContainer}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      />
+      <Marker draggable={true} position={{lon: point.point.x, lat: point.point.y}}
+              onMoveEnd={this.markerOpendataMoved.bind(this)}><Popup>Opendata</Popup></Marker>
+      <Rectangle bounds={bbox}/>
+      {this.renderCirclesOverpass()}
+    </Map>
+
   }
 
   render({qid, pid}, {point, overpass}) {
     return <div className={style.point}>
       {point ? this.renderPoint(point) : <Loader/>}
       <div>
-        <h2>Overpass</h2>
-        <div>
+        <h2>Rapprochement</h2>
+        <div className={style.renderElement}>
           <label htmlFor="radius">Radius</label>
-          <input id="radius" type="number" step={20} value={this.state.radius} onChange={(e) => this.setState({radius: e.target.value})}/>
+          <input id="radius" type="number" step={20} value={this.state.radius}
+                 onChange={(e) => this.setState({radius: e.target.value})}/>
           <button onClick={this.fetchOverpass.bind(this)}>Conflation</button>
-          {this.state.loaderOverpass ? <Loader/> : null}
-          <div><i>{this.getOverpassQuery()}</i></div>
         </div>
+        <div>
+          <div><i>{this.getOverpassQuery().split('\n').map(t => [t, <br/>])}</i></div>
+          {point ? this.renderMap(point) : null}
+        </div>
+      </div>
+      <div>
+        <h2>Overpass {this.state.loaderOverpass ? <Loader/> : null}</h2>
         {overpass ? this.renderOverpass(overpass) : null}
       </div>
       <div>
