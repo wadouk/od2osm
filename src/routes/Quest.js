a3061516f6e6b3d945547fd2baa3fcbcbab96acf
Nicolas Bétheuil <wadouk@gmail.com>
2020-05-12 17:34:05 +0200

    amélioration du listing des points

--- src/routes/Quest.js
+++ src/routes/Quest.js
@@ -1,6 +1,18 @@
 import {Link} from 'preact-router'
 import {useEffect} from 'preact/hooks'
 import {useContextReducer} from '../reducer'
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import fr from 'dayjs/locale/fr'
+
+dayjs.extend(localizedFormat)
+
+const filters = {
+  todo: 'à faire',
+  valid: 'En cours: modification',
+  create: 'En cours: création',
+  done: 'Traité',
+}
 
 export default function Quest({id}) {
   const [state, dispatch] = useContextReducer()
@@ -15,7 +27,7 @@ export default function Quest({id}) {
 
   function renderPoint({name, pid, qid, action, inserted, osmid, cid}) {
     function displayStatus() {
-      return cid ? `status: ${action} on ${inserted} ${osmid ? `osmid=${osmid}` : ''}` : ''
+      return cid ? `${filters[action]} depuis le ${dayjs(inserted).locale('fr').format('LLLL')} ${osmid ? `osmid=${osmid}` : ''}` : ''
     }
 
     return <li>
@@ -28,26 +40,19 @@ export default function Quest({id}) {
     dispatch({type: 'filterAction', msg: {filterAction: e.target.value}})
   }
 
-  function buildInput({v, l}) {
+  function buildInput([v, l]) {
     return <div>
       <input type="radio" name="action" value={v} id={v} onChange={filter} checked={filterAction === v}/>
       <label htmlFor={v}>{l}</label>
     </div>
   }
 
-  const filters = [
-    {v: 'todo', l: 'à faire'},
-    {v: 'valid', l: 'Rapprochés'},
-    {v: 'create', l: 'En cours de création'},
-    {v: 'done', l: 'Traité'},
-  ].map(buildInput)
-
   return (<div>
     <div>
       <h1>Les {points.length} points dans ce jeu de données OpenData</h1>
       <div>
-        <div>Afficher ceux</div>
-        {filters}
+        <div>Afficher ceux :</div>
+        {Object.entries(filters).map(buildInput)}
       </div>
     </div>
     <ul>{points.filter(({action}) => (filterAction && action === filterAction) || (!filterAction && !action)).map(renderPoint)}</ul>
